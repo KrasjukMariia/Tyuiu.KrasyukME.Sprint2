55ba4a4d6a3e230f501b217e52cfceb153c25044
KrasyukME <Tyuiu.KrasyukME@outlook.com>
2024-10-17 19:25:11 +0500

    Task1

--- Tyuiu.KrasyukME.Sprint2.Task1.V11.Lib/DataService.cs
+++ Tyuiu.KrasyukME.Sprint2.Task1.V11.Lib/DataService.cs
@@ -9,9 +9,9 @@ namespace Tyuiu.KrasyukME.Sprint2.Task1.V11.Lib
             results[0] = a > b;
             results[1] = a < d;
             results[2] = (a == c) | (b > c);
-            results[3] = (b != d) & (a <= b);
-            results[4] = (c >= d) ^ (a < b);
-            results[5] = !(a == b);
+            results[3] = (b == d) & (c > a);
+            results[4] = (a < c) ^ (b > d);
+            results[5] = a != b;
 
             return results;
         }
