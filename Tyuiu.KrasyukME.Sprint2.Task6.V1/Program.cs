a1ee405bfe40b627a6907547a0571163cfebd327
KrasyukME <Tyuiu.KrasyukME@outlook.com>
2024-10-17 23:05:18 +0500

    Task6

---
+++ Tyuiu.KrasyukME.Sprint2.Task6.V1/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.KrasyukME.Sprint2.Task6.V1.Lib;
+namespace Tyuiu.KrasyukME.Sprint2.Task6.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Красюк.М.Е. | ИБКСБ-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                  Спринт #2                              *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("*                              Задание #6                                 *");
+            Console.WriteLine("*                              Вариант #1                                 *");
+            Console.WriteLine("* Выполнил: Красюк Мария Евгеньевна    | ИБКСБ-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите номер месяца: ");
+            int numMonth = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+            if ((numMonth < 1) || (numMonth > 12))
+            {
+                res = "Введено неверное значение!";
+            }
+            else
+            {
+                res = "Количество дней в этом месяце: " + ds.FindMonthDaysCount(numMonth);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
