0fbea3fd273e05f147ed3101cdc719b95f63230a
KrasyukME <Tyuiu.KrasyukME@outlook.com>
2024-10-17 22:53:36 +0500

    Task5

---
+++ Tyuiu.KrasyukME.Sprint2.Task5.V3/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.KrasyukME.Sprint2.Task5.V3.Lib;
+namespace Tyuiu.KrasyukME.Sprint2.Task5.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Красюк.М.Е. | ИБКСБ-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                  Спринт #2                              *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("*                              Задание #5                                 *");
+            Console.WriteLine("*                              Вариант #3                                 *");
+            Console.WriteLine("* Выполнил: Красюк Мария Евгеньевна    | ИБКСБ-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите номер дня :");
+            int numDay = Convert.ToInt32(Console.ReadLine());
+            string res;
+
+            if ((numDay < 1) || (numDay > 7))
+            {
+                res = "Введенно неверное значение";
+            }
+            else
+            {
+                res = "Это " + ds.FindDayName(numDay);
+            }
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
