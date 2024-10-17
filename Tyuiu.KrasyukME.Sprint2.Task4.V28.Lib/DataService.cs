858fa0cf5e65073630293259abc3ae572c1dc1cd
KrasyukME <Tyuiu.KrasyukME@outlook.com>
2024-10-17 22:26:32 +0500

    Task4

--- Tyuiu.KrasyukME.Sprint2.Task4.V28.Lib/DataService.cs
+++ Tyuiu.KrasyukME.Sprint2.Task4.V28.Lib/DataService.cs
@@ -6,8 +6,8 @@ namespace Tyuiu.KrasyukME.Sprint2.Task4.V28.Lib
     {
         public double Calculate(double x, double y)
         {
-            
-                double z = x < y * 2 - 10 ? (Math.Pow((1 + (y + 2) / (x * x)), x)) : ((2 * (x * x) - Math.Cos(Math.Pow(y,2) + 12) / (y * y - Math.Sin(x * x) + 9)));
+
+                double z = x < y * 2 - 10 ? (Math.Pow((1 + (y + 2) / (x * x)), x)) : ((2 * x * x - Math.Cos(y) * Math.Cos(y) + 12) / (y * y - Math.Sin(x * x) + 9));
                 return Math.Round(z, 3);
             
 
